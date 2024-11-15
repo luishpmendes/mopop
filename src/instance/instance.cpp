908c584ca8d0da49a510586e71b2c1f6eadd1d71
Luis H. P. Mendes <luishpmendes@gmail.com>
2024-11-15 09:36:02 -0300

    Refactor Instance class for improved code readability and formatting

--- src/instance/instance.cpp
+++ src/instance/instance.cpp
@@ -7,61 +7,70 @@
 
 namespace mopop {
 
-void Instance::load_instance(const std::string &expected_returns_filename, const std::string &covariance_filename) {
-    std::ifstream expected_returns_file(expected_returns_filename), covariance_file(covariance_filename);
-    std::string line;
+void Instance::load_instance(const std::string &expected_returns_filename,
+                             const std::string &covariance_filename) {
+  std::ifstream expected_returns_file(expected_returns_filename),
+      covariance_file(covariance_filename);
+  std::string line;
 
-    if (!expected_returns_file.is_open()) {
-        throw std::runtime_error("Unable to open returns file");
+  if (!expected_returns_file.is_open()) {
+    throw std::runtime_error("Unable to open returns file");
+  }
+
+  std::getline(expected_returns_file, line);
+  this->tickers.clear();
+
+  while (std::getline(expected_returns_file, line)) {
+    std::istringstream linestream(line);
+    std::string ticker, expected_return_str;
+    double expected_return;
+
+    if (std::getline(linestream, ticker, ',') &&
+        std::getline(linestream, expected_return_str, ',')) {
+      this->tickers.push_back(ticker);
+      expected_return = std::stod(expected_return_str);
+      this->expected_returns.push_back(expected_return);
+    }
+  }
+
+  expected_returns_file.close();
+
+  if (!covariance_file.is_open()) {
+    throw std::runtime_error("Unable to open covariance file");
+  }
+
+  std::getline(covariance_file, line);
+  this->covariance_matrix.clear();
+
+  while (std::getline(covariance_file, line)) {
+    std::istringstream linestream(line);
+    std::string ticker;
+    std::vector<double> row;
+    std::string value_str;
+
+    std::getline(linestream, ticker, ',');
+
+    while (std::getline(linestream, value_str, ',')) {
+      row.push_back(std::stod(value_str));
     }
 
-    std::getline(expected_returns_file, line);
-    this->tickers.clear();
+    this->covariance_matrix.push_back(row);
+  }
 
-    while (std::getline(expected_returns_file, line)) {
-        std::istringstream linestream(line);
-        std::string ticker, expected_return_str;
-        double expected_return;
-
-        if (std::getline(linestream, ticker, ',') && std::getline(linestream, expected_return_str, ',')) {
-            this->tickers.push_back(ticker);
-            expected_return = std::stod(expected_return_str);
-            this->expected_returns.push_back(expected_return);
-        }
-    }
-
-    expected_returns_file.close();
-
-    if (!covariance_file.is_open()) {
-        throw std::runtime_error("Unable to open covariance file");
-    }
-
-    std::getline(covariance_file, line);
-    this->covariance_matrix.clear();
-
-    while (std::getline(covariance_file, line)) {
-        std::istringstream linestream(line);
-        std::string ticker;
-        std::vector<double> row;
-        std::string value_str;
-
-        std::getline(linestream, ticker, ',');
-
-        while (std::getline(linestream, value_str, ',')) {
-            row.push_back(std::stod(value_str));
-        }
-
-        this->covariance_matrix.push_back(row);
-    }
-
-    covariance_file.close();
-    this->num_assets = tickers.size();
-    this->senses = {NSBRKGA::Sense::MAXIMIZE, NSBRKGA::Sense::MINIMIZE};
+  covariance_file.close();
+  this->num_assets = tickers.size();
+  this->senses = {NSBRKGA::Sense::MAXIMIZE, NSBRKGA::Sense::MINIMIZE};
 }
 
-Instance::Instance() : num_assets(0), tickers(), expected_returns(), covariance_matrix(), senses() {}
+Instance::Instance()
+    : num_assets(0),
+      tickers(),
+      expected_returns(),
+      covariance_matrix(),
+      senses() {}
 
-Instance::Instance(const std::vector<std::string> &tickers, const std::vector<double> &expected_returns,
+Instance::Instance(const std::vector<std::string> &tickers,
+                   const std::vector<double> &expected_returns,
                    const std::vector<std::vector<double>> &covariance_matrix)
     : num_assets(covariance_matrix.size()),
       tickers(tickers),
@@ -69,35 +78,41 @@ Instance::Instance(const std::vector<std::string> &tickers, const std::vector<do
       covariance_matrix(covariance_matrix),
       senses({NSBRKGA::Sense::MAXIMIZE, NSBRKGA::Sense::MINIMIZE}) {}
 
-Instance::Instance(const std::string &returns_filename, const std::string &covariance_filename)
-    : num_assets(0), tickers(), expected_returns(), covariance_matrix(), senses() {
-    this->load_instance(returns_filename, covariance_filename);
+Instance::Instance(const std::string &returns_filename,
+                   const std::string &covariance_filename)
+    : num_assets(0),
+      tickers(),
+      expected_returns(),
+      covariance_matrix(),
+      senses() {
+  this->load_instance(returns_filename, covariance_filename);
 }
 
 Instance::Instance(const Instance &instance) = default;
 
 std::ostream &operator<<(std::ostream &os, const Instance &instance) {
-    os << "Number of assets: " << instance.num_assets << std::endl;
+  os << "Number of assets: " << instance.num_assets << std::endl;
 
-    os << "Tickers and Expected returns: " << std::endl;
+  os << "Tickers and Expected returns: " << std::endl;
 
-    for (size_t i = 0; i < instance.tickers.size(); ++i) {
-        os << instance.tickers[i] << ": " << instance.expected_returns[i] << std::endl;
+  for (size_t i = 0; i < instance.tickers.size(); ++i) {
+    os << instance.tickers[i] << ": " << instance.expected_returns[i]
+       << std::endl;
+  }
+
+  os << std::endl;
+
+  os << "Covariance matrix:" << std::endl;
+
+  for (const auto &row : instance.covariance_matrix) {
+    for (const auto &value : row) {
+      os << value << " ";
     }
 
     os << std::endl;
+  }
 
-    os << "Covariance matrix:" << std::endl;
-
-    for (const auto &row : instance.covariance_matrix) {
-        for (const auto &value : row) {
-            os << value << " ";
-        }
-
-        os << std::endl;
-    }
-
-    return os;
+  return os;
 }
 
-}  // namespace mopop
+}
--- src/instance/instance.hpp
+++ src/instance/instance.hpp
@@ -10,31 +10,34 @@
 
 namespace mopop {
 class Instance {
-   public:
-    unsigned num_assets;
+ public:
+  unsigned num_assets;
 
-    std::vector<std::string> tickers;
+  std::vector<std::string> tickers;
 
-    std::vector<double> expected_returns;
+  std::vector<double> expected_returns;
 
-    std::vector<std::vector<double>> covariance_matrix;
+  std::vector<std::vector<double>> covariance_matrix;
 
-    std::vector<NSBRKGA::Sense> senses;
+  std::vector<NSBRKGA::Sense> senses;
 
-   private:
-    void load_instance(const std::string& expected_returns_filename, const std::string& covariance_filename);
+ private:
+  void load_instance(const std::string& expected_returns_filename,
+                     const std::string& covariance_filename);
 
-   public:
-    Instance(const std::vector<std::string>& tickers, const std::vector<double>& expected_returns,
-             const std::vector<std::vector<double>>& covariance_matrix);
+ public:
+  Instance(const std::vector<std::string>& tickers,
+           const std::vector<double>& expected_returns,
+           const std::vector<std::vector<double>>& covariance_matrix);
 
-    Instance(const std::string& expected_returns_filename, const std::string& covariance_file);
+  Instance(const std::string& expected_returns_filename,
+           const std::string& covariance_file);
 
-    Instance(const Instance& instance);
+  Instance(const Instance& instance);
 
-    Instance();
+  Instance();
 
-    friend std::ostream& operator<<(std::ostream& os, const Instance& instance);
+  friend std::ostream& operator<<(std::ostream& os, const Instance& instance);
 };
 
-}  // namespace mopop
+}
